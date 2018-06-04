93408ec318cc0c3c46fb60ed11c5e021fe80714e
ILonewolf <richito1922@hotmail.com>
2018-06-04 09:13:55 -0500

    API

---
+++ API/routes/api.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+var api = express.Router();
+
+api.get("/users",(req,res)=>{
+    res.send("<h1>Estoy en API -users</h1>");
+});
+
+api.get("/numeros/:min/max",(req,res)=>{
+    var min = parseInt(req.params.min);
+    var max = parseInt(req.params.max);
+
+    if(isNaN(min)||isNaN(max)){
+        res.status(400);
+        res.json({
+            error:"Bad request"
+        });
+
+        return;
+    }
+    var result = Math.round((Math.random()*(max-min))+min);
+    res.json({
+        result:result
+    });
+});
